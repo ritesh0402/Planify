ad8a54ec548a1d4991d335cd7a5e03eed5bc9338
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2024-06-26 02:52:13 +0530

    Added Mongo Session store

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,51 @@
 import express from 'express'
 import session from 'express-session'
-import https from 'https'
 import cors from 'cors'
 import MongoStore from 'connect-mongo'
 import helmet from 'helmet'
-import path from 'path'
 import connectToMongo from './utils/db'
 import 'dotenv/config'
+
+import authRouter from './routers/authRouter'
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const store = MongoStore.create({
+   mongoUrl: process.env.MONGO_URI,
+   touchAfter: 24 * 60 * 60,
+   crypto: { secret: 'codeword' }
+})
+
+store.on("error", function (err) {
+   console.log('store error')
+   console.log(err)
+})
+
+
+const sessionConfig = {
+   resave: false,
+   saveUninitialized: false,
+   secret: process.env.SESSION_SECRET as string,
+   cookie: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" as any,
+      expires: new Date(Date.now() + (100 * 60 * 60 * 24 * 7)),
+      maxAge: (100 * 60 * 60 * 24 * 7),
+      httpOnly: true,
+   },
+   store
+};
 
 app.use(helmet());
 app.use(express.json());
 app.use(cors({ credentials: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sessionConfig))
 
-
+app.use('/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
    connectToMongo();
-   console.log(`Server listening on port: ${PORT}`);
+   console.log(`Server listening on http://localhost:${PORT}`);
 })
